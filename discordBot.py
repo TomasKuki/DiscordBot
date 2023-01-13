bb36466721a13caae45478f52f8344b5f9d4a86f
Tomás Matos <58147144+TomasKuki@users.noreply.github.com>
2023-01-13 01:17:22 +0000

    Update discordBot.py
    Modify the player function to show the player rank
    Add a player_mastery function that list the ten higher mastery champions but without full champion support

--- discordBot.py
+++ discordBot.py
@@ -68,13 +68,50 @@ async def translate(ctx, text, from_lang, to_lang):
 async def player(ctx, player_name):
     url = f'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{player_name}?api_key={RIOT_API_KEY}'
     response = requests.get(url)
-    if response.status_code == 200:
-        data = json.loads(response.text)
-        name = data['name']
-        level = data['summonerLevel']
-        await ctx.send(f'Player name: {name}, Level: {level}')
-    else:
+    if response.status_code != 200:
         await ctx.send(f'Unable to find player {player_name}')
+        return
+    player_data = response.json()
+    level = player_data['summonerLevel']
+    response = requests.get(f'https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/{player_data["id"]}?api_key={RIOT_API_KEY}')
+    if response.status_code != 200:
+        await ctx.send("An error occured, check the player name and region")
+        return
+    rank_data = response.json()
+    rank = rank_data[0]['rank'] if rank_data else "Unranked"
+    await ctx.send(f'Level: {level}\nRank: {rank}')
+
+@bot.command()
+async def player_mastery(ctx, player_name):
+    response = requests.get(f'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{player_name}?api_key={RIOT_API_KEY}')
+    if response.status_code != 200:
+        await ctx.send("An error occured, check the player name and region")
+        return
+    player_data = response.json()
+    player_id = player_data["id"]
+    response = requests.get(f'https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{player_id}?api_key={RIOT_API_KEY}')
+    if response.status_code != 200:
+        await ctx.send("An error occured, check the player name and region")
+        return
+    mastery_data = response.json()
+    mastery_data = sorted(mastery_data, key=lambda x: x["championPoints"], reverse=True)
+    mastery_str = ''
+    i=0
+    for mastery in mastery_data:
+        # load json data from file
+        with open('LolChampions.json', 'r') as file:
+            data = json.load(file)
+
+            try:
+                str(mastery["championId"]) == data[str(mastery["championId"])]
+                champName = data[str(mastery["championId"])]['name']
+                mastery_str += f'{champName} - Mastery: {mastery["championLevel"]} - Mastery Points: {mastery["championPoints"]}\n'
+            except KeyError:
+                champName = 'Name unavailable'
+        i += 1
+        if i == 10:
+            break
+    await ctx.send(f'{player_name} Mastery Champions: \n{mastery_str} \n Please note that support for all champions is not yet available!')
 
 
 bot.run('Your_Bot_Token')
